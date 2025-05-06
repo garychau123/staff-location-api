3c5e9ee49b2857098fe5b52294162201c23a0ee4
GaryChau1 <s4005858@student.rmit.edu.au>
2025-05-06 23:21:26 +1000

    staffdetails.java added with constructor, getter and setter methods

---
+++ src/main/java/staff/location/api/StaffDetails.java
@@ -0,0 +1,48 @@
+package staff.location.api;
+
+public class StaffDetails {
+    private String name;
+    private String id;
+    private String officeLocation;
+    private String officePhone;
+
+    public StaffDetails(String name, String id, String officeLocation, String officePhone) {
+        this.name = name;
+        this.id = id;
+        this.officeLocation = officeLocation;
+        this.officePhone = officePhone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOfficeLocation() {
+        return officeLocation;
+    }
+
+    public void setOfficeLocation(String officeLocation) {
+        this.officeLocation = officeLocation;
+    }
+
+    public String getOfficePhone() {
+        return officePhone;
+    }
+
+    public void setOfficePhone(String officePhone) {
+        this.officePhone = officePhone;
+    }
+}
+
