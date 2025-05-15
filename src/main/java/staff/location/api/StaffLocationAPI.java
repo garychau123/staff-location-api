d94bc1fecae4a43aad5cf22c4e1dc0b128686ea7
Gary Chau <s4005858@student.rmit.edu.au c:; cd 'c:\Users\William\github-classroom\rmit-computing-technologies\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23'; & 'C:\Program Files\Eclipse Adoptium\jdk-17.0.6.10-hotspot\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\William\AppData\Roaming\Code\User\workspaceStorage\cb4ffdbb4a40a87e648d353c4132f54f\redhat.java\jdt_ws\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23_9e8e4cd8\bin' 'coreq.GitPractice>
2025-05-15 12:30:12 +1000

    added staff-details endpoint with responseEntity

--- src/main/java/staff/location/api/StaffLocationAPI.java
+++ src/main/java/staff/location/api/StaffLocationAPI.java
@@ -1,9 +1,15 @@
 package staff.location.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
@@ -14,6 +20,13 @@ public class StaffLocationAPI {
         return "hi";
     }
 
+    @GetMapping("/staff-details/{firstName}") 
+    public ResponseEntity<List<StaffDetails>> getStaffDetails (@PathVariable String firstName) {
+        String url = "http://localhost:8080/employees/" + firstName;
+        return ResponseEntity.ok(new ArrayList<>());
+        }
+        
+    
     public static void main(String[] args){
         SpringApplication.run(StaffLocationAPI.class, args);
     }
