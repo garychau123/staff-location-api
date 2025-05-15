9db94e06fe6f26b8ee1afc71230755663ebb95bc
Gary Chau <s4005858@student.rmit.edu.au c:; cd 'c:\Users\William\github-classroom\rmit-computing-technologies\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23'; & 'C:\Program Files\Eclipse Adoptium\jdk-17.0.6.10-hotspot\bin\java.exe' '-XX:+ShowCodeDetailsInExceptionMessages' '-cp' 'C:\Users\William\AppData\Roaming\Code\User\workspaceStorage\cb4ffdbb4a40a87e648d353c4132f54f\redhat.java\jdt_ws\cosc2803-apr23-workshop-3-team-244-cosc2803-apr23_9e8e4cd8\bin' 'coreq.GitPractice>
2025-05-15 13:00:51 +1000

    added Employee.java gets necessary fields

---
+++ src/main/java/staff/location/api/Employee.java
@@ -0,0 +1,18 @@
+package staff.location.api;
+
+public class Employee {
+    private String id;
+    private String firstName;
+    private String surname;
+    private String officeLocation;
+    private String officePhone;
+
+    public Employee() {}
+
+    public String getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getSurname() { return surname; }
+    public String getOfficeLocation() { return officeLocation; }
+    public String getOfficePhone() { return officePhone; }
+}
+
